7f309806cb89c3fcdf74d239a273ad981c89da28
Deonte <dkill51@gmail.com>
2019-06-18 18:16:32 -0400

    Initial Commit

---
+++ CardViewAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardViewAnimation
+//
+//  Created by Deonte on 6/18/19.
+//  Copyright © 2019 Deonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
